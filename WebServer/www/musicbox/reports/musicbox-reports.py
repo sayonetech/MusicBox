1210fd99119a93bf46c919a35c25db4c28ba87f6
David Bianco <me@davidbianco.net>
2014-09-23 14:58:58 -0700

    added debug to flask

--- WebServer/www/musicbox/reports/musicbox-reports.py
+++ WebServer/www/musicbox/reports/musicbox-reports.py
@@ -8,13 +8,13 @@ SECRET_KEY = 'fjl34f4984fkl43#(p'
 SERVER_NAME = 'davidbianco.net'
 
 app = Flask(__name__)
-#app.config.from_object('wedding_site.app.config')
+app.debug = True
 
 #if not app.debug:
 if True:
     import logging
 
-    file_handler = logging.FileHandler('/var/log/flask/weberror.log')
+    file_handler = logging.FileHandler('/var/log/flask/musicbox-reports/weberror.log')
     file_handler.setLevel(logging.WARNING)
     app.logger.addHandler(file_handler)
 
