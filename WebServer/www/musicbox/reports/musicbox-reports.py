1d209237187ffb75d536cf0d88ee269faf52bf05
David Bianco <me@davidbianco.net>
2014-09-23 20:57:55 -0700

    changes to reveal slides

--- WebServer/www/musicbox/reports/musicbox-reports.py
+++ WebServer/www/musicbox/reports/musicbox-reports.py
@@ -27,27 +27,27 @@ if True:
 connection = happybase.Connection('cluster.davidbianco.net')
 
 
-@app.route("/api/v1/info/song/<event>/<time>")
+@app.route('/api/v1/info/song/<event>/<time>')
 def get_song_info(event=False, time=False):
     pass
 
-@app.route("/api/v1/info/artist/<event>/<time>")
+@app.route('/api/v1/info/artist/<event>/<time>')
 def get_artist_info(event=False, time=False):
     pass
 
-@app.route("/api/v1/info/users/demog/<gender>/<zipcode>/<age_bracket>")
+@app.route('/api/v1/info/users/demog/<gender>/<zipcode>/<age_bracket>')
 def get_user_info_demog(event=False, time=False):
     pass
 
-@app.route("/api/v1/info/users/<active_top>/<time>")
+@app.route('/api/v1/info/users/<active_top>/<time>')
 def get_user_info_counts(active_top=False, time=False):
     pass
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('reports.html')
 
-@app.route("/api/v1/doc")
+@app.route('/api/v1/doc')
 def doc():
     return render_template('doc.html')
 
@@ -55,7 +55,7 @@ def doc():
 def page_not_found(e):
     return jsonify(error="Invalid API request.")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001, debug=DEBUG)
 
 
