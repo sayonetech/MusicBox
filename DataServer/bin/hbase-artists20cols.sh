1d131c1073f40ab1af274ef504093a3174fea969
David Bianco <me@davidbianco.net>
2014-09-25 03:09:10 -0700

    added hbase-artists20cols.sh

---
+++ DataServer/bin/hbase-artists20cols.sh
@@ -0,0 +1,4 @@
+
+hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.columns=info:track_id,info:artist_familiarity,info:artist_hotttnesss,HBASE_ROW_KEY,info:artist_latitude,info:artist_location,info:artist_longitude,info:artist_name,info:artist_terms,info:artist_terms_freq,info:artist_terms_weight,info:danceability,info:duration,info:energy,info:release,info:similar_artists,info:song_hotttnesss,info:title,info:track_7digitalid,info:year artist_info_20cols msd/static-all/songs_20cols
+
+
