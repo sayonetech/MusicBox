d4ef9157602a95a5fe779862e043766c0977286f
David Bianco <me@davidbianco.net>
2014-09-26 01:00:25 -0700

    edit hbase-songsearch

--- DataServer/bin/hbase-songsearch.sh
+++ DataServer/bin/hbase-songsearch.sh
@@ -1,6 +1,8 @@
 
-hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.columns=HBASE_ROW_KEY,info:artist_familiarity,info:artist_hotttnesss,info:artist_id,info:artist_latitude,info:artist_location,info:artist_longitude,info:artist_name,info:artist_terms,info:artist_terms_freq,info:artist_terms_weight,info:danceability,info:duration,info:energy,info:release,info:similar_artists,info:song_hotttnesss,info:title,info:track_7digitalid,info:year artist_search msd/static-all/artistsearch
+hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.columns=HBASE_ROW_KEY,info:track_id,info:artist_familiarity,info:artist_hotttnesss,info:artist_id,info:artist_latitude,info:artist_location,info:artist_longitude,info:artist_name,info:artist_terms,info:artist_terms_freq,info:artist_terms_weight,info:danceability,info:duration,info:energy,info:release,info:similar_artists,info:song_hotttnesss,info:title,info:track_7digitalid,info:year artist_search msd/static-all/artistsearch
 
-hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.columns=HBASE_ROW_KEY,info:artist_familiarity,info:artist_hotttnesss,info:artist_id,info:artist_latitude,info:artist_location,info:artist_longitude,info:artist_name,info:artist_terms,info:artist_terms_freq,info:artist_terms_weight,info:danceability,info:duration,info:energy,info:release,info:similar_artists,info:song_hotttnesss,info:title,info:track_7digitalid,info:year song_search msd/static-all/songsearch
+echo "*NEXT*"
+
+hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.columns=HBASE_ROW_KEY,info:track_id,info:artist_familiarity,info:artist_hotttnesss,info:artist_id,info:artist_latitude,info:artist_location,info:artist_longitude,info:artist_name,info:artist_terms,info:artist_terms_freq,info:artist_terms_weight,info:danceability,info:duration,info:energy,info:release,info:similar_artists,info:song_hotttnesss,info:title,info:track_7digitalid,info:year song_search msd/static-all/songsearch
 
 
